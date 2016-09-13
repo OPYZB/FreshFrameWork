6795ac5486cbb45a06e64ab5ff664701d93288ab
CHERISH <qq@simpleli.local>
2016-09-13 09:48:47 +0800

    Initial Commit

---
+++ FreshFrameWorkTests/FreshFrameWorkTests.m
@@ -0,0 +1,39 @@
+//
+//  FreshFrameWorkTests.m
+//  FreshFrameWorkTests
+//
+//  Created by qq on 16/9/13.
+//  Copyright © 2016年 yuncheda. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FreshFrameWorkTests : XCTestCase
+
+@end
+
+@implementation FreshFrameWorkTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
